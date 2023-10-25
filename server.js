b187235f14d72839646fb1192362529224e0532e
Guadeloupe33 <stevenfrancius@gmail.com>
2023-10-24 22:10:40 -0400

    added server to the file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd(); // Get the current working directory
+
+const PORT = 3001;
+const app = express();
+
+// Middleware: Parse incoming requests with JSON payloads
+app.use(express.json());
+
+// Middleware: Parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// Mount your routes
+app.use(routes);
+
+// MongoDB connection event listener
+db.once("open", () => {
+  // Start the Express server and listen on the specified port
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
